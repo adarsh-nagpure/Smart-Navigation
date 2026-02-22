d9c5a11d4962d13151e6e9834732c8aaa1d5ce54
adarsh-nagpure <aadrshnagpure750@gmail.com>
2026-02-22 19:47:26 +0530

    update

---
+++ script.js
@@ -0,0 +1,22 @@
+const items = document.querySelectorAll('.nav li');
+const indicator = document.querySelector('.indicator');
+const circleIcon = document.querySelector('.circle');  
+
+
+items.forEach((item, index) => {
+  item.addEventListener('click', () => {
+
+    document.querySelector('.nav li.active')
+    .classList.remove('active');
+    item.classList.add('active');
+
+    indicator.style.left = `${index * 75 + 22}px`;
+
+    circleIcon.innerHTML = item.querySelector('.nav-icon').innerHTML;
+  });
+});
+ 
+indicator.style.top = "-40px";
+indicator.style.left = "30px";
+circleIcon.innerHTML =
+ document.querySelector('.nav li.active .nav-icon').innerHTML;
